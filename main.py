e648546048e027ffa2ecb72f2bb00cff6fef4006
Leyder Quintero <desarrollador9@wedevel.co>
2024-04-02 22:10:01 -0500

    Turtle practice OOP

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color('coral')
+timmy.forward(100)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
